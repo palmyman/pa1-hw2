e50e045fa8e1180d60ba26f596e0ec7617e460ec
Lubomír Čuhel <cuhel.lubomir@gmail.com>
2013-10-26 01:10:53 +0200

    Structure of application is done. Time to implement function evalPhi

--- main.c
+++ main.c
@@ -8,11 +8,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int inputCheck(int n, int check) {
+    if (check != 1 || n < 0) {
+        printf("Nespravny vstup.\n"); //Invalid input
+        return 0;
+    }
+    return 1;
+}
+
+int readN() {
+    int n, check;
+    printf("Zadejte n:\n");
+    check = scanf("%d", &n);
+    if(!inputCheck(n, check))
+        return 0;
+    return n;
+}
+
+int evalPhi(int n) {
+    int phi;
+    phi = 0;
+    
+    return phi;
+}
+
 /*
  * 
  */
 int main(int argc, char** argv) {
-
+    int n, phi;
+    n = readN();
+    if(!n) return 0;
+    phi = evalPhi(n);
+    printf("phi(%d) = %d\n", n, phi);
     return (EXIT_SUCCESS);
 }
 
