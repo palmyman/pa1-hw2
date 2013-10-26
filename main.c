f8368df9cd15baadd8dacf3a83db38da904493c8
Lubomír Čuhel <cuhel.lubomir@gmail.com>
2013-10-26 15:58:05 +0200

    divideWhilePossible function implemented. Divides argument by n till its possible, returns bool true if it was possible at least one time

--- main.c
+++ main.c
@@ -20,16 +20,30 @@ int readN() {
     int n, check;
     printf("Zadejte n:\n");
     check = scanf("%d", &n);
-    if(!inputCheck(n, check))
+    if (!inputCheck(n, check))
         return 0;
     return n;
 }
 
+int divideWhilePossible(int * arg, int n) {
+    int possible = 0;
+    while (!(* arg % n)) {
+        * arg /= n;
+        possible = 1;
+    }
+    return possible;
+}
+
 int evalPhi(int n) {
-    int phi;
-    phi = 0;
-    
-    return phi;
+    int phi, i;
+    phi = n;
+    i = 2;
+    //    while(1) {
+    //        if(n %= i) phi *= ((i-1)) / (i);
+    //    }
+    divideWhilePossible(&n, 2);
+
+    return n;
 }
 
 /*
@@ -38,7 +52,7 @@ int evalPhi(int n) {
 int main(int argc, char** argv) {
     int n, phi;
     n = readN();
-    if(!n) return 0;
+    if (!n) return 0;
     phi = evalPhi(n);
     printf("phi(%d) = %d\n", n, phi);
     return (EXIT_SUCCESS);
